f5840fb19e2f380ba684ab4d047bf1df8eb801e9
myps4 <myps4accaunt2001@gmail.com>
2022-05-23 17:08:56 +0300

    /registartion work

--- spring-boot-cloud-authentication-service/src/main/java/com/javadeveloperzone/controllers/UserController.java
+++ spring-boot-cloud-authentication-service/src/main/java/com/javadeveloperzone/controllers/UserController.java
@@ -2,13 +2,17 @@ package com.javadeveloperzone.controllers;
 
 import com.javadeveloperzone.entity.User;
 import com.javadeveloperzone.payroll.UserRequest;
+import com.javadeveloperzone.repo.RoleRepository;
 import com.javadeveloperzone.repo.UserRepository;
-import com.javadeveloperzone.service.CustomUserDetailService;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -17,14 +21,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class UserController {
 
 
-//    final CustomUserDetailService userDetailService;
+    //    final CustomUserDetailService userDetailService;
     final UserRepository userRepository;
     final PasswordEncoder bCryptPasswordEncoder;
 
-    public UserController(UserRepository userRepository, PasswordEncoder bCryptPasswordEncoder) {
+    final RoleRepository roleRepository;
+
+    public UserController(UserRepository userRepository, PasswordEncoder bCryptPasswordEncoder, RoleRepository roleRepository) {
 //        this.userDetailService = userDetailService;
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.roleRepository = roleRepository;
     }
 
 
@@ -35,19 +42,35 @@ public class UserController {
 
     @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody UserRequest newUser) {
+        User user = User.builder()
+                .username(newUser.getUsername())
+                .password(bCryptPasswordEncoder.encode(newUser.getPassword()))
+                .roles(List.of(roleRepository.findById(1L).get())).build();
         EntityModel<User> entityUser = EntityModel.of(
-                userRepository.save(User.builder()
-                        .username(newUser.getUsername())
-                        .password(newUser.getPassword()).build()));
+                userRepository.save(user),
+                linkTo(methodOn(UserController.class).userOne(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).userAll()).withRel("users"));
         return ResponseEntity //
                 .created(entityUser.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
                 .body(entityUser);
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public EntityModel<User> userOne(@PathVariable Long id) {
         User user = userRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Пользователь c id: " + id + " не найден"));
-        return EntityModel.of(user, linkTo(methodOn(UserController.class).userOne(id)).withSelfRel());
+        return EntityModel.of(user, linkTo(methodOn(UserController.class).userOne(id)).withSelfRel(),
+                linkTo(methodOn(UserController.class).userAll()).withRel("users"));
+    }
+
+    @GetMapping("/users")
+    CollectionModel<EntityModel<User>> userAll() {
+        List<EntityModel<User>> users = userRepository.findAll().stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).userOne(user.getId())).withSelfRel(),
+                        linkTo(methodOn(UserController.class).userAll()).withRel("users")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(users, linkTo(methodOn(UserController.class).userAll()).withSelfRel());
     }
 }
